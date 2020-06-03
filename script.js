2c9f0dd73a7f5e7f6ccc4d7fb6d7bdab36bb453c
Sreejith <osreejith11@gmail.com>
2020-06-03 20:50:25 +0530

    Snake is added

--- script.js
+++ script.js
@@ -3,3 +3,31 @@ const ctx = canvas.getContext("2D");
 const canvasSize = 600;
 canvas.width = canvasSize;
 canvas.height = canvasSize;
+
+const snakeBox = 20;
+const totalMoves = canvasSize/snakeBox;
+
+const apple = new Image();
+apple.src ='images/apple.png';
+
+let dead = new Audio();
+let eat = new Audio();
+let up = new Audio();
+let down = new Audio();
+let left = new Audio();
+let right = new Audio();
+
+dead.src = 'audio/dead.mp3';
+eat.src = 'audio/eat.mp3';
+up.src = 'audio/up.mp3';
+down.src = 'audio/down.mp3';
+left.src = 'audio/left.mp3';
+right.src = 'audio/right.mp3';
+
+// Defining Snake
+let snake = [];
+snake[0] = {
+    x : 9 * snakeBox ,
+    y : 10 * snakeBox
+};
+
